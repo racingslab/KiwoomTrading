91c4d3b8f3a8d84ca1aed14fea403ecbcbd2092e
kukekyakya <gmlwo308@gmail.com>
2019-12-27 14:49:09 +0900

    조건검색목록조회, 조건검색별 종목 조회, 주식 잔고 조회

---
+++ KiwoomTrading/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace KiwoomTrading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.conditionDataGridView = new System.Windows.Forms.DataGridView();
+            this.조건식_조건식번호 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.조건식_조건식이름 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stockDataGridView = new System.Windows.Forms.DataGridView();
+            this.조건검색_종목코드 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.조건검색_종목명 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.조건검색_현재가 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.조건검색_전일대비 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.조건검색_등락율 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.조건검색_거래량 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.search_button = new System.Windows.Forms.Button();
+            this.balanceDataGridView = new System.Windows.Forms.DataGridView();
+            this.balance_button = new System.Windows.Forms.Button();
+            this.잔고_종목코드 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.잔고_종목명 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.잔고_보유수량 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.잔고_매입금액 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.잔고_현재가 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.잔고_손익금액 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.잔고_손익율 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.conditionDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.balanceDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // conditionDataGridView
+            // 
+            this.conditionDataGridView.AllowUserToAddRows = false;
+            this.conditionDataGridView.AllowUserToDeleteRows = false;
+            this.conditionDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.conditionDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.조건식_조건식번호,
+            this.조건식_조건식이름});
+            this.conditionDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.conditionDataGridView.Name = "conditionDataGridView";
+            this.conditionDataGridView.RowHeadersVisible = false;
+            this.conditionDataGridView.RowHeadersWidth = 51;
+            this.conditionDataGridView.RowTemplate.Height = 27;
+            this.conditionDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.conditionDataGridView.Size = new System.Drawing.Size(228, 274);
+            this.conditionDataGridView.TabIndex = 0;
+            // 
+            // 조건식_조건식번호
+            // 
+            this.조건식_조건식번호.HeaderText = "조건식번호";
+            this.조건식_조건식번호.MinimumWidth = 6;
+            this.조건식_조건식번호.Name = "조건식_조건식번호";
+            this.조건식_조건식번호.Width = 110;
+            // 
+            // 조건식_조건식이름
+            // 
+            this.조건식_조건식이름.HeaderText = "조건식이름";
+            this.조건식_조건식이름.MinimumWidth = 6;
+            this.조건식_조건식이름.Name = "조건식_조건식이름";
+            this.조건식_조건식이름.Width = 110;
+            // 
+            // stockDataGridView
+            // 
+            this.stockDataGridView.AllowUserToAddRows = false;
+            this.stockDataGridView.AllowUserToDeleteRows = false;
+            this.stockDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.stockDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.조건검색_종목코드,
+            this.조건검색_종목명,
+            this.조건검색_현재가,
+            this.조건검색_전일대비,
+            this.조건검색_등락율,
+            this.조건검색_거래량});
+            this.stockDataGridView.Location = new System.Drawing.Point(246, 12);
+            this.stockDataGridView.Name = "stockDataGridView";
+            this.stockDataGridView.RowHeadersVisible = false;
+            this.stockDataGridView.RowHeadersWidth = 51;
+            this.stockDataGridView.RowTemplate.Height = 27;
+            this.stockDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.stockDataGridView.Size = new System.Drawing.Size(695, 274);
+            this.stockDataGridView.TabIndex = 1;
+            // 
+            // 조건검색_종목코드
+            // 
+            this.조건검색_종목코드.HeaderText = "종목코드";
+            this.조건검색_종목코드.MinimumWidth = 6;
+            this.조건검색_종목코드.Name = "조건검색_종목코드";
+            this.조건검색_종목코드.Width = 125;
+            // 
+            // 조건검색_종목명
+            // 
+            this.조건검색_종목명.HeaderText = "종목명";
+            this.조건검색_종목명.MinimumWidth = 6;
+            this.조건검색_종목명.Name = "조건검색_종목명";
+            this.조건검색_종목명.Width = 125;
+            // 
+            // 조건검색_현재가
+            // 
+            this.조건검색_현재가.HeaderText = "현재가";
+            this.조건검색_현재가.MinimumWidth = 6;
+            this.조건검색_현재가.Name = "조건검색_현재가";
+            this.조건검색_현재가.Width = 125;
+            // 
+            // 조건검색_전일대비
+            // 
+            this.조건검색_전일대비.HeaderText = "전일대비";
+            this.조건검색_전일대비.MinimumWidth = 6;
+            this.조건검색_전일대비.Name = "조건검색_전일대비";
+            this.조건검색_전일대비.Width = 125;
+            // 
+            // 조건검색_등락율
+            // 
+            this.조건검색_등락율.HeaderText = "등락율";
+            this.조건검색_등락율.MinimumWidth = 6;
+            this.조건검색_등락율.Name = "조건검색_등락율";
+            this.조건검색_등락율.Width = 125;
+            // 
+            // 조건검색_거래량
+            // 
+            this.조건검색_거래량.HeaderText = "거래량";
+            this.조건검색_거래량.MinimumWidth = 6;
+            this.조건검색_거래량.Name = "조건검색_거래량";
+            this.조건검색_거래량.Width = 125;
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(12, 472);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(100, 50);
+            this.axKHOpenAPI1.TabIndex = 2;
+            this.axKHOpenAPI1.OnReceiveTrData += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEventHandler(this.AxKHOpenAPI1_OnReceiveTrData);
+            // 
+            // search_button
+            // 
+            this.search_button.Location = new System.Drawing.Point(12, 304);
+            this.search_button.Name = "search_button";
+            this.search_button.Size = new System.Drawing.Size(75, 23);
+            this.search_button.TabIndex = 3;
+            this.search_button.Text = "검색";
+            this.search_button.UseVisualStyleBackColor = true;
+            // 
+            // balanceDataGridView
+            // 
+            this.balanceDataGridView.AllowUserToAddRows = false;
+            this.balanceDataGridView.AllowUserToDeleteRows = false;
+            this.balanceDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.balanceDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.잔고_종목코드,
+            this.잔고_종목명,
+            this.잔고_보유수량,
+            this.잔고_매입금액,
+            this.잔고_현재가,
+            this.잔고_손익금액,
+            this.잔고_손익율});
+            this.balanceDataGridView.Location = new System.Drawing.Point(246, 304);
+            this.balanceDataGridView.Name = "balanceDataGridView";
+            this.balanceDataGridView.RowHeadersVisible = false;
+            this.balanceDataGridView.RowHeadersWidth = 51;
+            this.balanceDataGridView.RowTemplate.Height = 27;
+            this.balanceDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.balanceDataGridView.Size = new System.Drawing.Size(691, 218);
+            this.balanceDataGridView.TabIndex = 4;
+            // 
+            // balance_button
+            // 
+            this.balance_button.Location = new System.Drawing.Point(128, 304);
+            this.balance_button.Name = "balance_button";
+            this.balance_button.Size = new System.Drawing.Size(75, 23);
+            this.balance_button.TabIndex = 5;
+            this.balance_button.Text = "잔고조회";
+            this.balance_button.UseVisualStyleBackColor = true;
+            this.balance_button.Click += new System.EventHandler(this.Balance_button_Click);
+            // 
+            // 잔고_종목코드
+            // 
+            this.잔고_종목코드.HeaderText = "종목코드";
+            this.잔고_종목코드.MinimumWidth = 6;
+            this.잔고_종목코드.Name = "잔고_종목코드";
+            this.잔고_종목코드.Width = 125;
+            // 
+            // 잔고_종목명
+            // 
+            this.잔고_종목명.HeaderText = "종목명";
+            this.잔고_종목명.MinimumWidth = 6;
+            this.잔고_종목명.Name = "잔고_종목명";
+            this.잔고_종목명.Width = 125;
+            // 
+            // 잔고_보유수량
+            // 
+            this.잔고_보유수량.HeaderText = "보유수량";
+            this.잔고_보유수량.MinimumWidth = 6;
+            this.잔고_보유수량.Name = "잔고_보유수량";
+            this.잔고_보유수량.Width = 125;
+            // 
+            // 잔고_매입금액
+            // 
+            this.잔고_매입금액.HeaderText = "매입금액";
+            this.잔고_매입금액.MinimumWidth = 6;
+            this.잔고_매입금액.Name = "잔고_매입금액";
+            this.잔고_매입금액.Width = 125;
+            // 
+            // 잔고_현재가
+            // 
+            this.잔고_현재가.HeaderText = "현재가";
+            this.잔고_현재가.MinimumWidth = 6;
+            this.잔고_현재가.Name = "잔고_현재가";
+            this.잔고_현재가.Width = 125;
+            // 
+            // 잔고_손익금액
+            // 
+            this.잔고_손익금액.HeaderText = "손익금액";
+            this.잔고_손익금액.MinimumWidth = 6;
+            this.잔고_손익금액.Name = "잔고_손익금액";
+            this.잔고_손익금액.Width = 125;
+            // 
+            // 잔고_손익율
+            // 
+            this.잔고_손익율.HeaderText = "손익율";
+            this.잔고_손익율.MinimumWidth = 6;
+            this.잔고_손익율.Name = "잔고_손익율";
+            this.잔고_손익율.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 534);
+            this.Controls.Add(this.balance_button);
+            this.Controls.Add(this.balanceDataGridView);
+            this.Controls.Add(this.search_button);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Controls.Add(this.stockDataGridView);
+            this.Controls.Add(this.conditionDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.conditionDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.balanceDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView conditionDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 조건식_조건식번호;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 조건식_조건식이름;
+        private System.Windows.Forms.DataGridView stockDataGridView;
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.Button search_button;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 조건검색_종목코드;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 조건검색_종목명;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 조건검색_현재가;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 조건검색_전일대비;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 조건검색_등락율;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 조건검색_거래량;
+        private System.Windows.Forms.DataGridView balanceDataGridView;
+        private System.Windows.Forms.Button balance_button;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 잔고_종목코드;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 잔고_종목명;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 잔고_보유수량;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 잔고_매입금액;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 잔고_현재가;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 잔고_손익금액;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 잔고_손익율;
+    }
+}
+
---
+++ KiwoomTrading/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KiwoomTrading
+{
+    public partial class Form1 : Form
+    {
+        List<ConditionObject> conditionObjectList;
+        List<stockBalance> stockBalanceList;
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            axKHOpenAPI1.OnEventConnect += onEventConnect;
+            axKHOpenAPI1.OnReceiveConditionVer += onReceiveConditionVer;
+            conditionDataGridView.SelectionChanged += conditionSearch;
+            axKHOpenAPI1.OnReceiveTrCondition += onReceiveTrCondition;
+            axKHOpenAPI1.OnReceiveTrData += onReceiveTrData;
+            search_button.Click += conditionSearch;
+
+            axKHOpenAPI1.CommConnect();
+            
+        }
+        public void onReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if (e.sRQName == "조건검색종목")
+            {
+                int count = axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName);
+
+
+                int conditionObjectIndex = -1;
+
+                if (conditionDataGridView.SelectedCells.Count > 0)
+                {
+                    int conditionIndex = conditionDataGridView.SelectedCells[0].RowIndex;
+                    string selectedConditionName = conditionDataGridView["조건식_조건식이름", conditionIndex].Value.ToString();
+                    conditionObjectIndex = conditionObjectList.FindIndex(o => o.conditionName == selectedConditionName);
+                }
+
+                conditionObjectList[conditionObjectIndex].stockItemList.Clear();
+                stockDataGridView.Rows.Clear();
+                for (int i = 0; i < count; i++)
+                {
+                    string stockCode = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "종목코드").Trim();
+                    string stockName = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "종목명").Trim();
+                    long currentPrice = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "현재가").Replace("-", ""));
+                    double upDownRate = double.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "등락율"));
+                    int netChange = int.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "전일대비"));
+                    long volume = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "거래량"));
+
+                    stockDataGridView.Rows.Add();
+                    stockDataGridView["조건검색_종목코드", i].Value = stockCode;
+                    stockDataGridView["조건검색_종목명", i].Value = stockName;
+                    stockDataGridView["조건검색_현재가", i].Value = String.Format("{0:#,###}", currentPrice);
+                    stockDataGridView["조건검색_전일대비", i].Value = netChange;
+                    stockDataGridView["조건검색_등락율", i].Value = upDownRate;
+                    stockDataGridView["조건검색_거래량", i].Value = String.Format("{0:#,###}", volume);
+
+                    if (conditionObjectIndex > -1)
+                    {
+                        conditionObjectList[conditionObjectIndex].stockItemList.Add(new StockItem(stockCode, stockName, currentPrice, upDownRate, netChange, volume));
+                    }
+                }
+            } else if(e.sRQName == "계좌평가현황요청")
+            {
+                
+                int count = axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName);
+                stockBalanceList = new List<stockBalance>();
+
+                balanceDataGridView.Rows.Clear();
+                for (int i = 0; i < count; i++)
+                {
+                    string stockCode = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "종목코드").TrimStart('0');
+                    string stockName = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "종목명").Trim();
+                    long number = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "보유수량"));
+                    long buyingMoney = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "매입금액"));
+                    long currentPrice = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "현재가").Replace("-", ""));
+                    long estimatedProfit = long.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "손익금액"));
+                    double estimatedProfitRate = double.Parse(axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, i, "손익율"));
+
+                    balanceDataGridView.Rows.Add();
+                    balanceDataGridView["잔고_종목코드", i].Value = stockCode;
+                    balanceDataGridView["잔고_종목명", i].Value = stockName;
+                    balanceDataGridView["잔고_보유수량", i].Value = number;
+                    balanceDataGridView["잔고_매입금액", i].Value = buyingMoney;
+                    balanceDataGridView["잔고_현재가", i].Value = currentPrice;
+                    balanceDataGridView["잔고_손익금액", i].Value = estimatedProfit;
+                    balanceDataGridView["잔고_손익율", i].Value = estimatedProfitRate;
+
+                    stockBalanceList.Add(new stockBalance(stockCode, stockName, number, String.Format("{0:#,###}", buyingMoney), String.Format("{0:#,###}", currentPrice), estimatedProfit, String.Format("{0:f2}", estimatedProfitRate)));
+                }
+            }
+        }
+        public void onReceiveTrCondition(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrConditionEvent e)
+        {
+            // 분당 1회 제한
+            if (e.strCodeList.Length > 0)
+            {
+                string stockCodeList = e.strCodeList.Remove(e.strCodeList.Length - 1);
+                int stockCount = stockCodeList.Split(';').Length;
+
+                if (stockCount <= 100)
+                {
+                    axKHOpenAPI1.CommKwRqData(stockCodeList, 0, stockCount, 0, "조건검색종목", "5100");
+                }
+            }
+            else if (e.strCodeList.Length == 0)
+            {
+                MessageBox.Show("검색된 종목이 없습니다.");
+            }
+        }
+        public void conditionSearch(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conditionDataGridView.SelectedCells.Count > 0)
+                {
+                    int rowIndex = conditionDataGridView.SelectedCells[0].RowIndex;
+                    string conditionName = conditionDataGridView["조건식_조건식이름", rowIndex].Value.ToString();
+                    int conditionIndex = int.Parse(conditionDataGridView["조건식_조건식번호", rowIndex].Value.ToString());
+                    axKHOpenAPI1.SendCondition("0156", conditionName, conditionIndex, 1);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message.ToString());
+            }
+        }
+        public void onReceiveConditionVer(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveConditionVerEvent e)
+        {
+            conditionObjectList = new List<ConditionObject>();
+            if (e.lRet == 1)
+            {
+                string conditionList = axKHOpenAPI1.GetConditionNameList().TrimEnd(';');
+                string[] conditionArray = conditionList.Split(';');
+
+                for (int i = 0; i < conditionArray.Length; i++)
+                {
+                    string[] condition = conditionArray[i].Split('^');
+                    conditionDataGridView.Rows.Add();
+                    conditionDataGridView["조건식_조건식번호", i].Value = condition[0];
+                    conditionDataGridView["조건식_조건식이름", i].Value = condition[1];
+
+                    conditionObjectList.Add(new ConditionObject(condition[0], condition[1]));
+
+                }
+            }
+        }
+        public void onEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if (e.nErrCode == 0)
+            {
+                axKHOpenAPI1.GetConditionLoad();
+            }
+        }
+
+        private void AxKHOpenAPI1_OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+
+        }
+
+        private void Balance_button_Click(object sender, EventArgs e)
+        {
+            string accountNumber = ""; // 계좌번호
+            string password = "";// 계좌비밀번호
+
+            axKHOpenAPI1.SetInputValue("계좌번호", accountNumber);
+            axKHOpenAPI1.SetInputValue("비밀번호", password);
+            axKHOpenAPI1.SetInputValue("상장폐지조회구분", "0");
+            axKHOpenAPI1.SetInputValue("비밀번호입력매체구분", "00");
+            axKHOpenAPI1.CommRqData("계좌평가현황요청", "opw00004", 0, "0004");
+        }
+    }
+    class ConditionObject
+    {
+        public string conditionIndex;
+        public string conditionName;
+        public List<StockItem> stockItemList;
+        public ConditionObject(string conditionIndex, string conditionName)
+        {
+            this.conditionIndex = conditionIndex;
+            this.conditionName = conditionName;
+            stockItemList = new List<StockItem>();
+        }
+    }
+    class StockItem
+    {
+        public string stockCode;
+        public string stockName;
+        public long currentPrice;
+        public double upDownRate;
+        public int netChange;
+        public long volume;
+        public StockItem(string stockCode, string stockName, long currentPrice, double upDownRate, int netChange, long volume)
+        {
+            this.stockCode = stockCode;
+            this.stockName = stockName;
+            this.currentPrice = currentPrice;
+            this.upDownRate = upDownRate;
+            this.netChange = netChange;
+            this.volume = volume;
+        }
+    }
+
+    class stockBalance
+    {
+        public string 종목코드 { get; set; }
+        public string 종목명 { get; set; }
+        public long 수량 { get; set; }
+        public string 매수금 { get; set; }
+        public string 현재가 { get; set; }
+        public long 평가손익 { get; set; }
+        public string 수익률 { get; set; }
+
+        public stockBalance() { }
+
+        public stockBalance(string 종목번호, string 종목명, long 수량, string 매수금, string 현재가, long 평가손익, string 수익률)
+        {
+            this.종목코드 = 종목번호;
+            this.종목명 = 종목명;
+            this.수량 = 수량;
+            this.매수금 = 매수금;
+            this.현재가 = 현재가;
+            this.평가손익 = 평가손익;
+            this.수익률 = 수익률;
+        }
+    }
+}
